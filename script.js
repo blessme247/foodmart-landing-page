408e2c44f0c7a005ea6f2d8722094c730ddd5d4b
Abraham Solabi <solabiabraham@gmail.com>
2022-05-03 20:29:13 -0700

    Added dark and light theme to the website Added carousel to the testimony section with swiperjs framework Added more images to the images folder for the testimony section

--- script.js
+++ script.js
@@ -1,44 +1,66 @@
 const themeButton = document.querySelector('.theme')
 const bodyWrapper  = document.querySelector('.body')
+const footer = document.querySelector('.footer')
+
 
 themeButton.addEventListener('click', () => {
     if(bodyWrapper.classList.contains('dark')) {
         bodyWrapper.classList.remove('dark')
         themeButton.innerText = ('Dark Mode')
-        themeButton.style.border = '2px solid gold'
+      
     }
     else {
         bodyWrapper.classList.add('dark')
         themeButton.innerText = ('Light Mode')
-        themeButton.style.border = '2px solid gold'
+
     }
 })
 
-const navMenu = document.querySelector('.nav-aside')
+
+
+const navMenu = document.querySelector('.nav-mobile')
+const navMenuDesktop = document.querySelector('.nav-desktop')
 const navToggler = document.querySelector('.nav-toggler')
 const logo = document.querySelector('.logo')
 const navLinks = document.querySelector('.nav-links')
-
+const learnMore = document.querySelector('.learn-more')
 
 navToggler.addEventListener('click',() => {
 if(navMenu.style.display != 'block' ) { /*i used != 'block' instead of ==='none' becasue it makes the hamburger button functional on one click*/
     navMenu.style.display = 'block'
-    navMenu.classList.add('open')
     navToggler.style.zIndex = '1'
-    navToggler.style.backgroundColor = 'inherit'
     navToggler.classList.add('active')
+    navToggler.style.backgroundColor = 'white'
+    learnMore.style.zIndex = '-9999'
+    themeButton.style.display = 'none'
+    navToggler.style.backgroundColor = 'inherit'
     logo.style.marginLeft = '.9rem'
     bodyWrapper.style.overflow = 'hidden' /* this disallows scrolling on the website when the navMenu is being displayed */
-
 }
 else{ 
     navMenu.style.display = 'none'
-    navMenu.classList.remove('open')
      navToggler.classList.remove('active')
+     navToggler.style.color = 'black'
+     navToggler.style.backgroundColor = 'white'
      logo.style.marginLeft = '1rem'
+     themeButton.style.display = 'block'
      bodyWrapper.style.overflow = 'visible'
 }
+
+})
+navToggler.addEventListener('click', () => {
+    if(navMenu.classList.contains('open')) {
+        navMenu.classList.remove('open')
+        navMenu.style.transform = 'translateX(100%)'
+    }
+    else {
+        navMenu.classList.add('open')
+        navMenu.style.transform = 'translateX(0%)'
+    }
 })
 
+const swiperSlide = document.querySelector('.swiper-slide')
+
+swiperSlide.style.width = '80%'
    
 
